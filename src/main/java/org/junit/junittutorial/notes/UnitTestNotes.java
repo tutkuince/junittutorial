84f9ae9ef3bde4fd931358a05f252d6160d0472e
tutkuince <tutku.ince@outlook.com>
2018-09-12 15:20:54 +0300

    Unit test notes

---
+++ src/main/java/org/junit/junittutorial/notes/UnitTestNotes.java
@@ -0,0 +1,17 @@
+package org.junit.junittutorial.notes;
+
+/**
+ * @author tutku
+ *
+ * @since Sep 12, 2018
+ */
+public class UnitTestNotes {
+	/**
+	 * 
+	 * given - an object under test initialization + stubs/mocks creation, stubbing and injection 
+	 * when - an operation to test in a given test 
+	 * then - received result assertion + mocks verification (if needed)
+	 * clear - 
+	 * 
+	 */
+}
