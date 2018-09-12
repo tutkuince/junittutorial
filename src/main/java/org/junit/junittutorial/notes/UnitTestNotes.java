7b099c6201ea8357bdf1d1ed1c1cb402a9f149d5
tutkuince <tutku.ince@outlook.com>
2018-09-12 15:23:19 +0300

    Michael Feathers's notes about Unit Test

--- src/main/java/org/junit/junittutorial/notes/UnitTestNotes.java
+++ src/main/java/org/junit/junittutorial/notes/UnitTestNotes.java
@@ -13,5 +13,19 @@ public class UnitTestNotes {
 	 * then - received result assertion + mocks verification (if needed)
 	 * clear - 
 	 * 
+	 * 
+	 * A test is not a unit test if:
+	 * 	1) It talks to the database
+	 * 	2) It communicates across the network
+	 * 	3) It touches the file system
+	 * 	4) It can't run correctly at the same time as any of your other unit tests
+	 * 	5) You have to do special things to your environment (such as editing config files) to run it.
+	 * 
+	 * Tests that do things things aren't bad. Often they are worth writing,
+	 * and they can be written in a unit test harness. However, it is
+	 * important to be able to separate them from true unit tests so that we
+	 * can keep a set of tests that we can run fast whenever we make our changes.
+	 * 
+	 *  Michael Feathers
 	 */
 }
