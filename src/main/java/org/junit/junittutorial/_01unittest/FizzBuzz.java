152e9635edc49ecc145c5e7cdf38250026cf7f1c
tutkuince <tutku.ince@outlook.com>
2018-09-12 15:48:20 +0300

    update

---
+++ src/main/java/org/junit/junittutorial/_01unittest/FizzBuzz.java
@@ -0,0 +1,15 @@
+package org.junit.junittutorial._01unittest;
+
+/**
+ * @author tutku
+ *
+ * @since Sep 12, 2018
+ */
+public class FizzBuzz {
+
+	public String stringFor(int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/org/junit/junittutorial/_01unittest/FizzBuzzTest.java
@@ -0,0 +1,21 @@
+package org.junit.junittutorial._01unittest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author tutku
+ *
+ * @since Sep 12, 2018
+ */
+class FizzBuzzTest {
+
+	@Test
+	void returnFizzWhenTheNumberIsDividedByThree() {
+		FizzBuzz fizzBuzz = new FizzBuzz();
+		
+		assertEquals("Fizz", fizzBuzz.stringFor(3));
+	}
+
+}
