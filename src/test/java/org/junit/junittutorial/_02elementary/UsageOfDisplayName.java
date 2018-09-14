36311b7538ee30d7c14504ae027ea0359ce81164
tutkuince <tutku.ince@outlook.com>
2018-09-14 13:32:46 +0300

    Usage of @DisplayName

---
+++ src/test/java/org/junit/junittutorial/_02elementary/UsageOfDisplayName.java
@@ -0,0 +1,25 @@
+package org.junit.junittutorial._02elementary;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Tutku Ince
+ *
+ * @since Sep 14, 2018
+ */
+@DisplayName("Calculator Tests")
+public class UsageOfDisplayName {
+
+	@Test
+	@DisplayName("* Addition of two integers *")
+	void additionOfTwoIntegers() {
+
+	}
+
+	@Test
+	@DisplayName("* Substraction of two integers *")
+	void substractionOfTwoIntegers() {
+
+	}
+}
