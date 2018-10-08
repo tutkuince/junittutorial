2b597134e7a2c59d590fda99684e54e329fa6b77
tutkuince <tutku.ince@outlook.com>
2018-10-08 19:31:12 +0300

    refactoring with new method namely findMinCostPath

--- src/main/java/org/junit/junittutorial/_02elementary/_03example/MinCostPath.java
+++ src/main/java/org/junit/junittutorial/_02elementary/_03example/MinCostPath.java
@@ -8,29 +8,38 @@ package org.junit.junittutorial._02elementary._03example;
 public class MinCostPath {
 
 	public int find(int[][] matrix, Cell start, Cell target) {
-		
+
 		validateIfTheCellIsOutOfMatrixBound(matrix, start);
 		validateIfTheCellIsOutOfMatrixBound(matrix, target);
-		
-		if(start.equals(target))
+
+		if (start.equals(target))
 			return matrix[start.getRow()][start.getColumn()];
-		
-		Cell currentCell = start;
-		int cost = 0;
-		while(!currentCell.equals(target)) {
-			cost += matrix[currentCell.getRow()][currentCell.getColumn()];
-			currentCell = new Cell(currentCell.getRow(), currentCell.getColumn() + 1);
-		}
-		cost += matrix[currentCell.getRow()][currentCell.getColumn()];
-		
-		return cost;
+
+		return findMinCostPath(matrix, start, target);
 	}
 
 	private void validateIfTheCellIsOutOfMatrixBound(int[][] matrix, Cell cell) {
-		if(cell.getRow() >= matrix.length || cell.getRow() < 0)
+		if (cell.getRow() >= matrix.length || cell.getRow() < 0)
 			throw new IllegalArgumentException();
-		if(cell.getColumn() >= matrix[0].length || cell.getColumn() < 0)
+		if (cell.getColumn() >= matrix[0].length || cell.getColumn() < 0)
 			throw new IllegalArgumentException();
+	}
+
+	private int findMinCostPath(int[][] matrix, Cell start, Cell target) {
+
+		if (start.getRow() > target.getRow() || start.getColumn() > target.getColumn())
+			return Integer.MAX_VALUE;
+
+		if (start.equals(target))
+			return matrix[start.getRow()][start.getColumn()];
+
+		int rightPathCost = findMinCostPath(matrix, new Cell(start.getRow(), start.getColumn() + 1), target);
+		int downPathCost = findMinCostPath(matrix, new Cell(start.getRow() + 1, start.getColumn()), target);
+		int diagonalPathCost = findMinCostPath(matrix, new Cell(start.getRow() + 1, start.getColumn() + 1), target);
+
+		final int min = Math.min(rightPathCost, Math.min(downPathCost, diagonalPathCost));
+
+		return min + matrix[start.getRow()][start.getColumn()];
 	}
 
 }
