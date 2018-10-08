9323faa08ecc9f5a68e2cd14dfa0c302d36bde09
tutkuince <tutku.ince@outlook.com>
2018-10-08 16:58:45 +0300

    Created class and gave info about out test cases

---
+++ src/test/java/org/junit/junittutorial/_02elementary/_03example/MinCostPathTest.java
@@ -0,0 +1,19 @@
+package org.junit.junittutorial._02elementary._03example;
+
+/**
+ * @author Tutku Ince
+ *
+ * @since Oct 8, 2018
+ */
+public class MinCostPathTest {
+
+	/**
+	 * matrix, int[][], start cell, target cell, Cell(row, column)
+	 * start cell or target cell is out of matrix bound
+	 * start cell equals target cell then return cost of start cell
+	 * find minimum cost path for one row matrix
+	 * find minimum cost path for multiple row matrix
+	 * right path cost, down path cost, diagonal path cost
+	 *  
+	 */
+}
