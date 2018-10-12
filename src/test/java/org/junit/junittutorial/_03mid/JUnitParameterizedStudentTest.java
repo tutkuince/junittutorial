d08879d5dffaaa8437e6700c9ab802a601114da2
tutkuince <tutku.ince@outlook.com>
2018-10-12 15:33:07 +0300

    Usage of TestInfo interface provide by JUnit 5

---
+++ src/test/java/org/junit/junittutorial/_03mid/JUnitParameterizedStudentTest.java
@@ -0,0 +1,44 @@
+package org.junit.junittutorial._03mid;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.junittutorial._02elementary._04example.model.Student;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+/**
+ * @author Tutku Ince
+ *
+ * @since Oct 12, 2018
+ */
+public class JUnitParameterizedStudentTest {
+	
+	private Student student;
+	
+	@BeforeEach
+	void setStudent(TestInfo testInfo) {
+		
+		if(testInfo.getTags().contains("create"))
+			student = new Student("1", "Tutku", "Ince");
+		else
+			student = new Student("2", "Ugur", "Batikan");
+
+	}
+	
+	@Test
+	@DisplayName("Create Student")
+	@Tag("create")
+	void createStudent() throws Exception {
+		assertEquals("Tutku", student.getName());
+	}
+	
+	@Test
+	@DisplayName("Add Course to Student")
+	@Tag("addCourse")
+	void addCourseToStudent() throws Exception {
+		assertEquals("Ugur", student.getName());
+	}
+}
