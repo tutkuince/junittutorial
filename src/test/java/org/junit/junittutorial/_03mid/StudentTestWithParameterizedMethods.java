7ef0c3f4ab9351a18d4b8d633f96b356ff2c58e3
tutkuince <tutku.ince@outlook.com>
2018-10-12 19:38:27 +0300

    usage of @EnumSource

--- src/test/java/org/junit/junittutorial/_03mid/StudentTestWithParameterizedMethods.java
+++ src/test/java/org/junit/junittutorial/_03mid/StudentTestWithParameterizedMethods.java
@@ -1,7 +1,10 @@
 package org.junit.junittutorial._03mid;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.util.Random;
 
 import org.junit.junittutorial._02elementary._04example.model.Course;
 import org.junit.junittutorial._02elementary._04example.model.LecturerCourseRecord;
@@ -12,6 +15,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 /**
@@ -34,8 +38,9 @@ public class StudentTestWithParameterizedMethods {
 			student = new Student("1", "Tutku", "Ince");
 		}
 
-		@ParameterizedTest
-		@ValueSource(strings = { "101", "103", "105" })	// run more than 1 times. There are 3 parameters in @ValueSource so it runs 3 times.
+		@ParameterizedTest	// Parameterized tests make it possible to run a test multiple times with different arguments.
+		//They are declared just like regular @Test methods but use the @ParameterizedTest annotation instead.
+		@ValueSource(strings = { "101", "103", "105" })	// 3 times (101, 103, 105) 
 		void addCourseToStudent(String courseCode) {
 			
 			final LecturerCourseRecord lecturerCourseRecord = new LecturerCourseRecord(new Course(courseCode), new Semester());
@@ -44,6 +49,29 @@ public class StudentTestWithParameterizedMethods {
 			assertEquals(studentCourseSize, student.getStudentCourseRecords().size());
 			assertTrue(student.isTakeCourse(new Course(courseCode)));
 		}
-
 	}
+	
+	@TestInstance(Lifecycle.PER_CLASS)
+	@Nested
+	class EnumSourceDemo {
+		
+		@BeforeAll
+		void setUp() {
+			student = new Student("1", "Tutku", "Ince");
+		}
+
+		@ParameterizedTest
+		@EnumSource(Course.CourseType.class)
+		void addCourseToStudent(Course.CourseType courseType) {
+			final Course course = Course.newCourse().withCode(String.valueOf(new Random().nextInt(200)))
+					.withCourseType(courseType).course();
+
+			final LecturerCourseRecord courseRecord = new LecturerCourseRecord(course, new Semester());
+			student.addCourse(courseRecord);
+			assertFalse(student.getStudentCourseRecords().isEmpty());
+			assertTrue(student.isTakeCourse(course));
+
+		}
+	}
+	
 }
