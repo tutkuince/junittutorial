5ee822c72182c0b6857f728229019d432679ff9a
tutkuince <tutku.ince@outlook.com>
2018-10-12 13:27:59 +0300

    Prime Factor example

---
+++ src/test/java/org/junit/junittutorial/_02elementary/_05example/PrimeFactorsTest.java
@@ -0,0 +1,30 @@
+package org.junit.junittutorial._02elementary._05example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Tutku Ince
+ *
+ * @since Oct 12, 2018
+ */
+public class PrimeFactorsTest {
+	
+	@Test
+	void findPrimeFactors() throws Exception {
+		PrimeFactors primeFactors = new PrimeFactors();
+		
+		assertThrows(IllegalArgumentException.class, () -> primeFactors.generate(1), () -> "Number must be greater than 2");
+		
+		assertAll("Prime Numbers", 
+				() -> assertEquals(Arrays.asList(2), primeFactors.generate(2)),
+				() -> assertEquals(Arrays.asList(3), primeFactors.generate(3)),
+				() -> assertEquals(Arrays.asList(2, 2, 3), primeFactors.generate(12)),
+				() -> assertEquals(Arrays.asList(5), primeFactors.generate(5)),
+				() -> assertEquals(Arrays.asList(2, 2, 5), primeFactors.generate(20))
+				);
+	}
+}
