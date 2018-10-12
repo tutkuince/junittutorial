fbe144d14c59613def50802ac2fd7a1309106e4d
tutkuince <tutku.ince@outlook.com>
2018-10-12 20:42:47 +0300

    usage of @ArgumentsSource

--- src/test/java/org/junit/junittutorial/_03mid/StudentTestWithParameterizedMethods.java
+++ src/test/java/org/junit/junittutorial/_03mid/StudentTestWithParameterizedMethods.java
@@ -16,8 +16,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -217,5 +220,50 @@ public class StudentTestWithParameterizedMethods {
 			assumingThat(courseCode.equals("105"),
 					() -> assertEquals(Course.CourseType.MANDATORY, courseType));
 		}
+	}
+
+	@TestInstance(Lifecycle.PER_CLASS)
+	@Nested
+	class ArgumentsSourceDemo {
+		private int studentCourseSize = 0;
+
+		@BeforeAll
+		void setUp() {
+			student = new Student("1", "Tutku", "Ince");
+		}
+
+		@ParameterizedTest
+		@ArgumentsSource(CourseCodeAndTypeProvider.class)
+		void addCourseToStudent(String courseCode, Course.CourseType courseType) {
+
+			final Course course = new Course(courseCode);
+			course.setCourseType(courseType);
+			
+			final LecturerCourseRecord lecturerCourseRecord = new LecturerCourseRecord(new Course(courseCode),
+					new Semester());
+			student.addCourse(lecturerCourseRecord);
+			studentCourseSize++;
+			assertEquals(studentCourseSize, student.getStudentCourseRecords().size());
+			assertTrue(student.isTakeCourse(new Course(courseCode)));
+			assumingThat(courseCode.equals("101"),
+					() -> assertEquals(Course.CourseType.MANDATORY, courseType));
+			assumingThat(courseCode.equals("103"),
+					() -> assertEquals(Course.CourseType.ELECTIVE, courseType));
+			assumingThat(courseCode.equals("105"),
+					() -> assertEquals(Course.CourseType.MANDATORY, courseType));
+		}
+	}
+	
+	static class CourseCodeAndTypeProvider implements ArgumentsProvider {
+
+		@Override
+		public Stream<? extends Arguments> provideArguments(ExtensionContext arg0) throws Exception {
+			return Stream.of(
+					Arguments.of("101", Course.CourseType.MANDATORY),
+					Arguments.of("103", Course.CourseType.ELECTIVE),
+					Arguments.of("105", Course.CourseType.MANDATORY)
+					);
+		}
+		
 	}
 }
