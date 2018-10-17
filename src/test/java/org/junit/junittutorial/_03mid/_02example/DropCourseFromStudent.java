5411faaaed183db032e9f5b442aef51c0d13da06
tutkuince <tutku.ince@outlook.com>
2018-10-17 14:10:19 +0300

    notActiveSemester()

--- src/test/java/org/junit/junittutorial/_03mid/_02example/DropCourseFromStudent.java
+++ src/test/java/org/junit/junittutorial/_03mid/_02example/DropCourseFromStudent.java
@@ -15,9 +15,10 @@ public class DropCourseFromStudent {
 	// throws illegal argument exception for null lecturer course record
 	// throws illegal argument exception if the student did't register course before
 	// throws not active semester exception if the semester is not active
-	// throws not active semester exception if the add drop period is closed for the semester
+	// throws not active semester exception if the add drop period is closed for the
+	// semester
 	// drop course from student
-	
+
 	private Semester addDropPeriodOpenSemester() {
 		final Semester activeSemester = new Semester();
 		final LocalDate semesterDate = LocalDate.of(activeSemester.getYear(), activeSemester.getTerm().getStartMonth(),
@@ -33,5 +34,10 @@ public class DropCourseFromStudent {
 		if (LocalDate.now().getMonthValue() == 1)
 			activeSemester.setAddDropPeriodInWeek(-1);
 		return activeSemester;
+	}
+
+	private Semester notActiveSemester() {
+		final Semester activeSemester = new Semester();
+		return new Semester(LocalDate.of(activeSemester.getYear() - 1, 1, 1));
 	}
 }
