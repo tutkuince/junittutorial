814d2767b33fe31cb9e900cec019cadb26844b62
tutkuince <tutku.ince@outlook.com>
2018-10-17 17:18:38 +0300

    RomanConverter

---
+++ src/main/java/org/junit/junittutorial/_03mid/romanconverter/RomanConverter.java
@@ -0,0 +1,27 @@
+package org.junit.junittutorial._03mid.romanconverter;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author Tutku Ince
+ *
+ * @since Oct 17, 2018
+ */
+public class RomanConverter {
+	private static Map<Integer, String> romanDigitMap = Map.of(50, "L", 40, "XL", 10, "X", 9, "IX", 5, "V", 4, "IV", 1, "I");
+
+	public static String convert(int number) {
+
+		StringBuilder result = new StringBuilder();
+		AtomicInteger atomicNumber = new AtomicInteger(number);
+		romanDigitMap.keySet().stream().sorted(Comparator.reverseOrder()).forEach(numberForRomanDigit -> {
+			while (atomicNumber.get() >= numberForRomanDigit) {
+				result.append(romanDigitMap.get(numberForRomanDigit));
+				atomicNumber.set(atomicNumber.get() - numberForRomanDigit);
+			}
+		});
+		return result.toString();
+	}
+}
