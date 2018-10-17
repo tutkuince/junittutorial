402f7d8080f43b28ee7bbf772f71835fdf7b8e96
tutkuince <tutku.ince@outlook.com>
2018-10-17 19:04:11 +0300

    added features to logger for showing instance's field

--- src/test/java/org/junit/junittutorial/_04advance/DropCourseTestInstancePostProcessor.java
+++ src/test/java/org/junit/junittutorial/_04advance/DropCourseTestInstancePostProcessor.java
@@ -1,5 +1,7 @@
 package org.junit.junittutorial._04advance;
 
+import java.util.logging.Logger;
+
 import org.junit.junittutorial._02elementary._04example.model.Student;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestInstancePostProcessor;
@@ -11,10 +13,14 @@ import org.junit.jupiter.api.extension.TestInstancePostProcessor;
  */
 public class DropCourseTestInstancePostProcessor implements TestInstancePostProcessor {
 
+	private static Logger logger = Logger.getLogger(DropCourseTestInstancePostProcessor.class.getName());
+	
 	@Override
 	public void postProcessTestInstance(Object testInstance, ExtensionContext context) throws Exception {
+		
 		final StudentTestWithTestInstancePostProcessor.DropCourseFromStudent dropCourseFromStudent = (StudentTestWithTestInstancePostProcessor.DropCourseFromStudent) testInstance;
 		dropCourseFromStudent.setStudent(new Student("1", "Tutku", "Ince"));
+		logger.info("Student " + dropCourseFromStudent.getStudent().getName() + " is provided!");
 	}
 
 }
