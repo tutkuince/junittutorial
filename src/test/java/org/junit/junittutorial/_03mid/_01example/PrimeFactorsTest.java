3b8a140580790bb025de65c3a759e12e2a7c2e36
tutkuince <tutku.ince@outlook.com>
2018-10-17 13:53:04 +0300

    Customized display name on generateWithParametirezedTest()

--- src/test/java/org/junit/junittutorial/_03mid/_01example/PrimeFactorsTest.java
+++ src/test/java/org/junit/junittutorial/_03mid/_01example/PrimeFactorsTest.java
@@ -60,7 +60,7 @@ public class PrimeFactorsTest {
 				PrimeFactors.generate(repetitionInfo.getCurrentRepetition()));
 	}
 
-	@ParameterizedTest
+	@ParameterizedTest(name = "Generate Prime Factor for {arguments}")
 	@ValueSource(ints = { 1, 2, 3, 4, 5, 6, 7, 8, 9 })
 	void generateWithParameterizedTest(Integer number) throws Exception {
 		assertEquals(primeFactorExpectations.get(number), PrimeFactors.generate(number));
