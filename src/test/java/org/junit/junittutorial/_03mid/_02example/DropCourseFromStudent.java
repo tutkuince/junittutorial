379dbcdbd4388eb614a25df3d4453d04a974584f
tutkuince <tutku.ince@outlook.com>
2018-10-17 14:34:28 +0300

    throws not active semester exception if the semester is not active

--- src/test/java/org/junit/junittutorial/_03mid/_02example/DropCourseFromStudent.java
+++ src/test/java/org/junit/junittutorial/_03mid/_02example/DropCourseFromStudent.java
@@ -1,17 +1,20 @@
 package org.junit.junittutorial._03mid._02example;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.DynamicContainer.*;
 import static org.junit.jupiter.api.DynamicTest.*;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import org.junit.junittutorial._02elementary._04example.model.Course;
 import org.junit.junittutorial._02elementary._04example.model.LecturerCourseRecord;
+import org.junit.junittutorial._02elementary._04example.model.NotActiveSemesterException;
 import org.junit.junittutorial._02elementary._04example.model.Semester;
 import org.junit.junittutorial._02elementary._04example.model.Student;
+import org.junit.junittutorial._02elementary._04example.model.StudentCourseRecord;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
@@ -34,12 +37,20 @@ public class DropCourseFromStudent {
 
 		final Student student = new Student("1", "Tutku", "Ince");
 
-		return Stream.of(dynamicTest("throws illegal argument exception for null lecturer course record", () -> {
-			assertThrows(IllegalArgumentException.class, () -> student.dropCourse(null));
+		return Stream.of(
+			dynamicTest("throws illegal argument exception for null lecturer course record", () -> {
+				assertThrows(IllegalArgumentException.class, () -> student.dropCourse(null));
 		}), dynamicTest("throws illegal argument exception if the student did't register course before", () -> {
-			final LecturerCourseRecord lecturerCourseRecord = new LecturerCourseRecord(new Course("101"), new Semester());
-			assertThrows(IllegalArgumentException.class, () -> student.dropCourse(lecturerCourseRecord));
+				final LecturerCourseRecord lecturerCourseRecord = new LecturerCourseRecord(new Course("101"), new Semester());
+				assertThrows(IllegalArgumentException.class, () -> student.dropCourse(lecturerCourseRecord));
+		}), dynamicTest("throws not active semester exception if the semester is not active", () -> {
+				final Semester notActiveSemester = notActiveSemester();
+				Assumptions.assumeTrue(!notActiveSemester().isActive());
+				final LecturerCourseRecord lecturerCourseRecord = new LecturerCourseRecord(new Course("101"), notActiveSemester);
+				Student stdUgur = new Student("1", "Ugur", "Batikan", Set.of(new StudentCourseRecord(lecturerCourseRecord)));
+				assertThrows(NotActiveSemesterException.class, () -> stdUgur.dropCourse(lecturerCourseRecord));
 		})
+				
 				);
 	}
 
